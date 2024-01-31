f274cfa9489e82f95b3762727e53b97ab60d215b
noemi3a <nhenrique@3anet.com.br>
2024-01-30 23:10:24 -0300

    Label com parâmetro

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ function App() {
     
     <div className="App">
       <Banner />
-      <CampoTexto />
+      <CampoTexto label="Nome" placeholder="Digite seu nome"/>
+      <CampoTexto label="Cargo" placeholder="Digite seu cargo"/>
+      <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem"/>
       
     </div>
   );
